e906fda9a3cd0a7b7877d4438afcb4d56d2dd319
WillSho1 <whshocon@gmail.com>
2023-10-23 13:03:17 -0400

    Additional checking functionality to AddCourse: prereqs, completed, enrollment, capacity

--- backend/database/lambdafunc/AddCourse.py
+++ backend/database/lambdafunc/AddCourse.py
@@ -14,10 +14,9 @@ def lambda_handler(event, context):
     user = event["UserID"]
     course = event["CourseID"]
     section = event["Section"]
-    
-    # Check if the user is already enrolled in the course
     course_id_section = f"{course}-{section}"
     
+    #CHECK: ENROLLMENT, PREREQS, COMPLETED
     user_item = client.get_item(
         TableName='Users',
         Key={
@@ -29,15 +28,57 @@ def lambda_handler(event, context):
             }
         }
     )
-    if 'Item' in user_item:
-        enrollment = user_item['Item'].get('Enrollment', {'L': []}).get('L', [])
-        clist = [item.get('S') for item in enrollment]
-        for classes in clist:
-            if course in classes:
+    subj = course.split()[0]
+    course_item = client.get_item(
+        TableName = 'Subjects',
+        Key = {
+            'subject': {
+                'S': subj,
+            },
+            'courseid': {
+                'S': course
+            }
+        }
+    )
+    if ('Item' in user_item):
+        if ('Item' in course_item):
+            prereqs = course_item['Item'].get('Prereqs', {'L': []}).get('L', [])
+            taken = user_item['Item'].get('Completed', {'L': []}).get('L', [])
+            enrollment = user_item['Item'].get('Enrollment', {'L': []}).get('L', [])
+            
+            clist = [item.get('S') for item in enrollment]
+            takenlist = [item.get('S') for item in taken]
+            prelist = [item.get('S') for item in prereqs]
+            #PREREQS
+            notin = []
+            for i in prelist:
+                if i not in takenlist:
+                    notin.append(i)
+            if len(notin) > 0:
                 return {
                     'statusCode': 400,
-                    'body': json.dumps(f'Already enrolled in {course_id_section}.')
+                    'body': json.dumps(f'Missing Prereqs: {notin}')
                 }
+            #TAKEN
+            for classes in takenlist:
+                if course in classes:
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps(f'Already completed {course}.')
+                    }
+            #ALREADY ENROLLED
+            for classes in clist:
+                if course in classes:
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps(f'Already enrolled in {course_id_section}.')
+                    }
+        else:
+            #should never enter this
+            return {
+                'statusCode': 400,
+                'body': json.dumps('Course not found')
+            }
     else:
         #should never enter this
         return {
@@ -45,7 +86,35 @@ def lambda_handler(event, context):
             'body': json.dumps('User not found')
         }
         
-    #UPDATE TABLES IF THE STUDENT IS NOT ALREADY ENROLLED
+    #CHECK: CAPACITY
+    course_item1 = client.get_item(
+        TableName='Courses',
+        Key={
+            'CourseID': {
+                'S': course,
+            },
+            'Section': {
+                'N': section        
+            }
+        }
+    )
+    if ('Item' in course_item1):
+        cap = course_item1['Item'].get('Capacity', {}).get('N', '0')
+        enrollcount = course_item1['Item'].get('Enrollment', {}).get('N', '0')
+        if cap == enrollcount:
+            return {
+                'statusCode': 400,
+                'body': json.dumps(f'{course_id_section} is currently full: {enrollcount}/{cap}.')
+            }
+    else:
+        return {
+            #should never entert this
+            'statusCode': 400,
+            'body': json.dumps(f'{course_id_section} not found')
+        }
+        
+        
+    #UPDATE TABLES IF PREVIOUS CONDITIONS PASS
     response = client.update_item(
         TableName='Courses',
         Key={
