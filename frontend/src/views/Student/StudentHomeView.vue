f5abdedf16af3448716ecab2d63f713aefec87b9
Yousif Said <yousifsaid@Yousifs-MacBook-Air.local>
2023-11-15 12:44:22 -0500

    Adding student home page

--- frontend/src/views/Student/StudentHomeView.vue
+++ frontend/src/views/Student/StudentHomeView.vue
@@ -1,30 +1,70 @@
 <template>
-    <header class="app-header">
+  <header class="app-header">
+    <div>
+      <h1>{{ title }}</h1>
+      <nav>
+        <ul>
+          <li>
+            <RouterLink to="/studentcs">Go to Course Search</RouterLink>
+          </li>
+        </ul>
+      </nav>
       <div>
-        <h1>{{ title }}</h1>
-        <nav>
-          <ul>
-            <li>
-              <RouterLink to="/studentcs">Go to Course Search</RouterLink>
-            </li>
-          </ul>
-        </nav>
+        <h2>Enrolled Courses:</h2>
+        <ul>
+          <li v-for="course in courses" :key="course.S">{{ course.S }}</li>
+        </ul>
+
       </div>
-    </header>
-  </template>
+    </div>
+  </header>
+</template>
   
-  <script setup>
-  // import the <RouterLink> component so that we can use it in the template above
-  import { RouterLink } from "vue-router";
-  
-  // give this component a title property so that the parent component (app.vue) can set whatever title it wants
-  defineProps({
-    title: {
-      type: String,
-      required: true,
-    },
+<script setup>
+import { onMounted, ref } from "vue";
+import { RouterLink } from "vue-router";
+
+// Define the reactive variables
+const title = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const courses = ref([]); // This will hold the list of courses
+const username = ref(''); // You should have some logic to populate this with the current user's ID
+
+// Function to fetch courses from the API
+function listCourses() {
+  let userId = username.value; // Make sure this is the actual UserID you need to send
+  let endpoint = `https://74ym2fsc17.execute-api.us-east-1.amazonaws.com/ProjAPI/studenthomepage/enrollment?UserID=${userId}`;
+
+  fetch(endpoint)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // If the Lambda function returns the body as a stringified JSON, you should parse it
+    courses.value = JSON.parse(data.body); // Update the courses reactive property
+    // If the Lambda function returns the body as an object, you can assign it directly
+    // courses.value = data.body; 
+  })
+  .catch(error => {
+    console.error('An error occurred:', error);
   });
-  </script>
+}
+
+// Call listCourses on component mount
+onMounted(() => {
+  // Assuming that username gets its value before this onMounted hook runs
+  listCourses();
+});
+</script>
+
   
   <style>
   /* give the header itself a background color, a border, and add some padding to the content */
