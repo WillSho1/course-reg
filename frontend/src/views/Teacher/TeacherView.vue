d9c69a1b7112130e1899b8fe857c6725726a3f32
Yousif Said <yousifsaid@Yousifs-MacBook-Air.local>
2023-11-27 20:54:28 -0500

    Added teacher homepage

--- frontend/src/views/Teacher/TeacherView.vue
+++ frontend/src/views/Teacher/TeacherView.vue
@@ -1,65 +1,137 @@
 <template>
-    <div>
-      <header class="app-header">
-        <div>
-          <h1>{{ title }}</h1>
-          <div>
-            <h2>Enrolled Courses:</h2>
-            <ul>
-              <li v-for="course in courses" :key="course">{{ course }}</li>
-            </ul>
-          </div>
-        </div>
-      </header>
+  <header class="app-header">
+    <div class="header-content">
+      <h1>{{ title }}</h1>
     </div>
-  </template>
+    <div class="course-list">
+      <h3>Below are the courses you are teaching this semester:</h3>
+      <ul>
+        <li v-for="course in courses" :key="course" class="course-item">
+          {{ course }}
+          <button @click="getCourseInfo(course)">Get Info</button>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+
   
-  <script>
-  export default {
-    data() {
-      return {
-        title: "Teacher Homepage",
-        courses: []
-      };
-    },
-    mounted() {
-      this.listCourses();
-    },
-    methods: {
-      listCourses() {
-        const userId = "YOUR_USER_ID"; // Replace with the actual user ID
-        const endpoint = `https://YOUR_API_GATEWAY_URL/teacherhomepage/enrollment?UserID=${userId}`;
-  
-        fetch(endpoint)
-          .then(response => {
-            if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-          })
-          .then(data => {
-            this.courses = data; // Assuming the Lambda function returns an array of course IDs
-          })
-          .catch(error => {
-            console.error('An error occurred:', error);
-          });
+<script setup>
+import { computed, onMounted, ref } from "vue";
+import useRoute from "vue-router";
+
+const title = computed(() => `Welcome ${username.value}`);
+
+const courses = ref([]); // This will hold the list of courses
+
+const route = useRoute(); // Initialize the route here
+const username = ref(route.query.userId); // Retrieve the passed username
+
+// Function to fetch courses from the API
+function listCourses() {
+  let userId = username.value;
+  let endpoint = `https://74ym2fsc17.execute-api.us-east-1.amazonaws.com/ProjAPI/teacherhomepage/enrollment?UserID=${userId}`;
+
+  console.log(endpoint);
+  fetch(endpoint)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.body);
+      courses.value = data.body; // Assuming the Lambda function returns the data in the body attribute
+    })
+    .catch(error => {
+      console.error('An error occurred:', error);
+    });
+}
+
+// Call listCourses when the component is mounted
+onMounted(() => {
+  listCourses();
+});
+
+
+
+async function getCourseInfo(courseIdSection) {
+  try {
+    const response = await fetch(`https://74ym2fsc17.execute-api.us-east-1.amazonaws.com/ProjAPI/teacherhomepage/enrollment/courseinfo?course-id-section=${courseIdSection}`, {
+      method: 'GET'
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log(data.body);
+      alert(JSON.stringify(data.body, null, 2)); // Display course info in a simple alert for now
+    } else {
+      throw new Error(data);
     }
-  };
-  </script>
-  
-  <style scoped>
-  /* give the header itself a background color, a border, and add some padding to the content */
-  .app-header {
-    background-color: #fcfcfc;
-    border-bottom: 1px solid #e0e0e0;
-    padding: 1rem;
+  } catch (error) {
+    console.error('Error fetching course info:', error);
   }
-  
-  /* make the title within the header a larger and bolder font */
-  .app-header h1 {
-    font-size: 2rem;
-    font-weight: bold;
-  }
-  </style>
-  
+}
+
+</script>
+
+
+<style>
+/* General page styles */
+body {
+  font-family: 'Arial', sans-serif;
+  color: #333;
+  margin: 0;
+  padding: 0;
+}
+
+/* Header styles */
+.app-header {
+  background-color: #005792;
+  color: #fff;
+  padding: 1rem 2rem;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+}
+.header-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+/* Course search styles */
+.course-search {
+  margin-top: 1rem;
+  padding: 0 2rem;
+}
+.course-search a {
+  color: #005792;
+  text-decoration: none;
+  font-weight: bold;
+  background-color: #fff;
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+}
+
+/* Course list styles */
+.course-list {
+  margin-top: 1rem;
+  padding: 0 2rem;
+}
+.course-list h3 {
+  color: white;
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+.course-item {
+  background-color: #e0e0e0;
+  border-radius: 5px;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem 0;
+}
+
+</style>
